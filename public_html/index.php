23f6ab31dd75abb9c00679d894ce2cfd25467291
Maria Rosado <mrosado2@cnm.edu>
2017-04-19 12:49:30 -0600

    copied index from solo para mi

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
 
 		<meta charset="UTF-8"/>
@@ -21,12 +21,13 @@
 		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
 		<![endif]-->
 		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
 		<!-- jQuery Form, Additional Methods, Validate -->
 		<script type="text/javascript"
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
@@ -46,241 +47,33 @@
 		<!--FONT AWESOME-->
 		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
 		<!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css/family=Vitasse Bold" rel="stylesheet">
 
-		<title>BlackCat Web development</title>
-		<title>My Personal Website</title>
+		<title>Branding</title>
+		<style type="text/css">
+			body{
+				background-image: url(..public_html/images/cnm_ingenuity_sandias.png);
+			}
+		</style>
 	</head>
 	<body class="sfooter">
-		<div class="sfooter-content">
-			<header>
+		<div class="sfooter-content"></div>
 
-				<!-- begin header and navbar-->
-				<nav class="nav-scroll navbar navbar-default navbar-fixed-top">
-					<div class="container-fluid">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span> class="icon-bar"></span>
-							</button>
-							<a class="navbar-brand page-scroll">| BlackCat Web Develoment |</a>
-						</div>
-
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav navbar-left">
-								<li><a class="page-scroll" href="#home">Home</a></li>
-								<li><a class="page-scroll" href="#about">About Me</a></li>
-								<li><a class="page-scroll" href="#portfolio">Portfolio</a></li>
-								<li><a class="page-scroll" href="#contact">Contact Me</a> </li>
-							</ul>
-						</div>
-					</div>
-				</nav>
-
-			</header>
-
-			<!-- begin welcome section -->
-			<section>
-				<div class="container">
-					<img src="..public_html/img/class="background"
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="jumbotron text-center">
-								<h1>Maria Rosado</h1>
-								<h2> Web Developer</h2>
-								<p class="lead"></p>
-							</div>
-						</div><!-- /.col-sm-12 -->
-					</div><!-- /.row -->
-				</div><!-- /.container -->
-			</section>
-
-			<!-- begin photo/text section -->
-			<a name="about"></a><!-- this is an "anchor" link -->
-			<section class="bg-gray">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-3">
-							<img src="../public_html/img/maria-profile.png"
-								  class="img-responsive img-thumbnail xs-center-block m-b-2">
-						</div>
-						<div class="col-sm-9">
-							<div class="About me"></div><h2>About Me</h2>
-							<p class="lead">Hi! My name is Maria Rosado. I am a Web Developer. Photography aficionado, with a love
-								for creating amazing Websites that look great and have seamless transitions from the eye catchers
-								to it effectiveness.
-							</p>
-							<p>Love to solve problems and help business to find was to use technology and get the most of it,
-								without loosing the human touch.</p>
-							<p>I am based in Albuquerque, New Mexico, and benefit of a multicultural background.
-								I was born in Mexico City where had an amazing job that send me to work in very importants events
-								like the first World Summit in Cancun and presidential campains.</p>
-							<p>Never missed the opportunity to visit museaums and historical locations from the Mayas in the south,
-								Aztecs ruins in central Mexico, to the Tarahumaras in the north.
-								Moved to New York where lived for 13 chilling winters and summers that were never enough to get to
-								visit all the stores and breath taking views on the Fall. There I worked with apparel companies
-								through an import, export, storage and transportation company where I help them to implement
-								technology they already had but were not using to full advantage.</p>
-							<p>Within the first month of my implementation of my suggested changes they save over $3,000.00 dollars.
-								That job also gave me the opportunity to work and learn from cultures from around the world.
-								Then moved to New Mexico to work at Intel. There I learned soo much about cutting edge technology
-								and manufacturing.
-								Now I am putting all that experience to the advantage of my customers.</p>
-						</div>
-					</div><!-- /.row -->
-				</div><!-- /.container -->
-			</section>
-
-			<!-- begin 4 column section -->
-
-			<a name="moar"></a><!-- this is an "anchor" link -->
-			<section>
-				<div class="container">
-					<div class="jumbotron text-center">
-						<div class="text-center m-b-2"></div><h2>Portfolio</h2></div>
-					<div class="text-Portfolio"
-					<div class="row">
-			</section>
-			<div class="col-sm-3">
-				<div class="text-center-m-b-2">
-					<img src="../public_html/img/icon-ddcb-lg.png" alt="ddcb" class="img-responsive img-circle img-thumbnail">
-					<h5>Aaaa-capstone</h5>
-					<p class="text-center m-b-2">Project</p>
-				</div>
-			</div><!-- /.col-sm-3 -->
-
-			<div class="col-sm-3">
-				<div class="text-center m-b-2">
-					<img src="../public_html/img/coming-soon-1.png" alt="coming-soon"
-						  class="img-responsive img-circle img-thumbnail">
-					<div>
-						<h3>Title 2</h3>
-						<p class="text-center m-b-2">Project</p>
-					</div>
-				</div>
-			</div><!-- /.col-sm-3 -->
-
-			<div class="col-sm-3">
-				<div class="text-center m-b-2">
-					<img src="../public_html/img/coming-soon-1.png" alt="coming-soon"
-						  class="img-responsive img-circle img-thumbnail">
-					<div>
-						<h3>Title 3</h3>
-						<p class="text-center m-b-2">Project</p>
-					</div>
-				</div>
-			</div><!-- /.col-sm-3 -->
-			<div class="col-sm-3">
-				<div class="text-center m-b-2">
-					<img src="../public_html/img/coming-soon-1.png" alt="coming-soom"
-						  class="img-responsive img-circle img-thumbnail">
-					<div>
-						<h3>Title 4</h3>
-						<p class="text-center m-b-2">Project</p>
-					</div>
-				</div>
-			</div><!-- /.col-sm-3 -->
-
-		</div><!-- /.row -->
-		</div><!-- /.container -->
-		</section>
-		</div><!-- /.sfooter-content -->
-		<!------------------------- CONTACT FORM------------------------------------->
-		<div id="contact" class="container section">
-			<h2 class="heading">Contact Me</h2>
+		<div class="container">
 			<div class="row">
-				<div class="col-md-3"></div>
-				<!--Begin Contact Form-->
-				<div class="col-md-6">
-					<form id="contact-form" action="php/mailer.php" method="POST" novalidate>
+				<div class="col-sm-offset-2 col-sm-8">
+					<h1>ingenuity</h1>
+					<form>
 						<div class="form-group">
-							<label for="name">Name <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-user" aria-hidden="true"></i>
-								</div>
-								<input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
-							</div>
+							<label for="exampleInputUsername">Username</label>
+							<input type="username" class="form-control" id="exampleInputUsername" placeholder="Username">
+							<div class="form-group">
+								<label for="exampleInputPassword">Password</label>
+								<input type="password" class="form-control" id="exampleInputPassword" placeholder="=Password">
 						</div>
-						<div class="form-group">
-							<label for="email">Email <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-envelope" aria-hidden="true"></i>
-								</div>
-								<input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="subject">Subject</label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-pencil" aria-hidden="true"></i>
-								</div>
-								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="message">Message <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="fa fa-comment" aria-hidden="true"></i>
-								</div>
-								<textarea class="form-control" rows="5" id="message" name="message"
-											 placeholder="Message (2000 characters max)"></textarea>
-							</div>
-						</div>
-
-						<!-- reCAPTCHA -->
-						<div class="g-recaptcha" data-sitekey="6Lftrw0UAAAAAJMEE3A0yzZF_6GENXFPUiJwA6Pl"></div>
-
-						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 					</form>
-				</div>
-				<div class="col-md-3"></div>
-				<!--empty area for form error/success output-->
-				<div class="row">
-					<div class="col-xs-12">
-						<div id="output-area"></div>
-					</div>
-				</div>
 			</div>
 		</div>
-		</main>
-		</div>
-		<!-- begin footer -->
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<!--TWITTER ICON-->
-						<a href="//twitter.com/BlackcatDevelop" target="_blank" class="btn btn-social-icon btn-twitter">
-							<span class="fa fa-twitter fa-2x"></span>
-						</a>
-						<!--GITHUB ICON-->
-						<a href="//github.com/mrosado2" target="_blank" class="btn btn-social-icon btn-github">
-							<span class="fa fa-github fa-2x"></span>
-						</a>
-						<!--LINKEDIN ICON-->
-						<a href="//https://www.linkedin.com/in/maria-rosado-bb1608132?trk=hp-identity-name" target="_blank"
-							class="btn btn-social-icon btn-linkedin">
-							<span class="fa fa-linkedin fa-2x"></span>
-						</a>
-						<div class="col-sm-12 text-center">
-							<p>&copy; Copyright Text Here :P</p>
-							<div class="small">
-								<a href="#top"><i class="fa fa-arrow-up"></i> Back to Top</a>
-							</div>
-						</div>
-					</div>
-				</div>
-		</footer>
-	</body>
+		</body>
 	</head>
 </html>
